a9febd9dc49996c1a6ee5db4e1aa388568a7232d
ddelfaus <ddelfaus@gmail.com>
2019-09-19 18:09:56 -0400

    MVP

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,177 @@
 // CODE here for your Lambda Classes
+
+
+
+class Person {
+    constructor(attributes){
+        this.name = attributes.name,
+        this.age = attributes.age,   
+        this.location = attributes.location;    
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}. `
+    }
+}//Person close
+
+
+class Instructor extends Person {
+    constructor(attributes){
+        super(attributes),
+        this.specialty = attributes.specialty,
+        this.favLanguage = attributes.favLanguage,   
+        this.phrase = attributes.phrase;    
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`
+    }  
+    grade(student, subject){
+        return `${student} receives a perfect score on ${subject}.`
+    }
+}// Instructor close
+
+
+class Student extends Person {
+    constructor(attributes){
+        super(attributes),
+        this.previousBackground = attributes.previousBackground,
+        this.className = attributes.className,   
+        this.favSubjects = attributes.favSubjects;    
+    }
+    listsSubjects(){
+        this.favSubjects.map(list => console.log(list));
+                
+    } 
+    PRAssignment(subject){
+        return `${this.name} has submitted a PR for ${subject}.`
+    }
+    sprintChallenge(subject){
+        return `${this.name} has begun sprint challenge on ${subject}.`
+    }
+
+}// Student close
+
+
+class ProjectManager extends Instructor {
+    constructor(attributes){
+        super(attributes),
+        this.gradClassName = attributes.gradClassName,
+        this.favInstructor = attributes.favInstructor;       
+    }
+    standUp(channel){
+        return `${this.name} announces to ${channel}, @channel standy times!`
+    }
+    debugsCode(student, subject){
+        return `${this.name} debugs ${student}'s code on ${subject} `
+    }
+
+}// ProjectManager
+
+
+/// Instructors
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+  const jose = new Instructor({
+    name: 'Jose',
+    location: 'Alaska',
+    age: 32,
+    favLanguage: 'Ruby',
+    specialty: 'Front-end',
+    catchPhrase: `I pity the fool`
+  });
+  const kate = new Instructor({
+    name: 'Kate',
+    location: 'UK',
+    age: 28,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Excellent!`
+  });
+
+//Students
+
+const sarah = new Student({
+    name: 'Sarah',
+    location: 'Denmark',
+    age: 22,
+    previousBackground: 'college student',
+    className: 'Web Dev',
+    favSubjects: [
+        'Html',
+        'CSS'
+    ],
+  });
+
+  const rob = new Student({
+    name: 'Rob',
+    location: 'USA',
+    age: 24,
+    previousBackground: 'NAVY',
+    className: 'Web Dev',
+    favSubjects: [
+        'Java',
+        'CSS'
+    ],
+  });
+
+  const amanda = new Student({
+    name: 'Amanda',
+    location: 'USA',
+    age: 20,
+    previousBackground: 'unemployed',
+    className: 'UX Design',
+    favSubjects: [
+        'Math',
+        'UX Stuff'
+    ],
+  });
+
+
+
+  //Project Managers
+
+  const kyle = new ProjectManager({
+    name: 'Kyle',
+    location: 'New York',
+    age: 26,
+    favLanguage: 'JavaScript',
+    specialty: 'UX Design',
+    catchPhrase: `Greetings`,
+    gradClassName: 'UX 2',
+    favInstructor: 'fred'  
+  });
+  
+  const jessica = new ProjectManager({
+    name: 'Jessica',
+    location: 'Texas',
+    age: 26,
+    favLanguage: 'react',
+    specialty: 'WEB',
+    catchPhrase: `Yolo`,
+    gradClassName: 'Web 15',
+    favInstructor: 'Kate'  
+  });
+
+
+
+// instructors
+  console.log(fred.speak());
+  console.log(kate.favLanguage);
+  console.log(fred.demo('Math'));
+  console.log(fred.grade('Frank', 'Math'));
+
+//students
+  rob.listsSubjects();
+  console.log(sarah.PRAssignment('Java'));
+  console.log(amanda.sprintChallenge('UI'));
+  console.log(sarah.previousBackground);
+
+  console.log(jessica.standUp('Cats'));
+  console.log(kyle.debugsCode('David', 'Java'));
+  console.log(kyle.favInstructor);
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,188 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+
+
+class GameObject {
+    constructor(characteristic){
+        this.createdAt = characteristic.createdAt,
+        this.name = characteristic.name,
+        this.dimensions = characteristic.dimensions ;
+    }
+
+    //methods
+    destroy(){
+        return `${this.name} was removed from the game.`;
+    }
+
+}//GameObject close
+
+
+class CharacterStats extends GameObject{
+    constructor(characteristic){
+       super(characteristic),
+        this.healthPoints = characteristic.healthPoints;
+    }
+    //methods
+    takeDamage(){
+        return `${this.name} took damage.`;
+  
+    }
+}//CharacerStats close
+ 
+class Humanoid extends CharacterStats{ 
+    constructor(characteristic) {
+        super(characteristic),
+        this.team = characteristic.team,
+        this.weapons = characteristic.weapons,
+        this.language = characteristic.language;
+    }
+
+    greet(){
+        return `${this.name} offers a greeting in ${this.language}`;
+    }
+  }
+  
+  
+  
+  
+    const mage = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+      },
+      healthPoints: 5,
+      name: 'Bruce',
+      team: 'Mage Guild',
+      weapons: [
+        'Staff of Shamalama',
+      ],
+      language: 'Common Tongue',
+    });
+  
+    const swordsman = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+      },
+      healthPoints: 15,
+      name: 'Sir Mustachio',
+      team: 'The Round Table',
+      weapons: [
+        'Giant Sword',
+        'Shield',
+      ],
+      language: 'Common Tongue',
+    });
+  
+    const archer = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+      },
+      healthPoints: 10,
+      name: 'Lilith',
+      team: 'Forest Kingdom',
+      weapons: [
+        'Bow',
+        'Dagger',
+      ],
+      language: 'Elvish',
+    });
+  
+    console.log(mage.createdAt); // Today's date
+    console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+    console.log(swordsman.healthPoints); // 15
+    console.log(mage.name); // Bruce
+    console.log(swordsman.team); // The Round Table
+    console.log(mage.weapons); // Staff of Shamalama
+    console.log(archer.language); // Elvish
+    console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+    console.log(mage.takeDamage()); // Bruce took damage.
+    console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  
+  
+    // Stretch task: 
+    // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+    // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+    // * Create two new objects, one a villain and one a hero and fight it out with methods!
+  
+  
+  
+    // function Villain(characteristic){
+      
+    //   Humanoid.call(this, characteristic);
+    // }
+    //   Villain.prototype = Object.create(Humanoid.prototype);
+     
+     
+     
+     
+    //   Villain.prototype.smack = function(character,damage){
+  
+    //     character.healthPoints = character.healthPoints - this.damage;
+  
+    //     return `${this.name} smacks ${character.name} for ${damage} damage!`;
+       
+    // };
+  
+   
+  
+  
+  
+  
+    // function Hero(characteristic){
+    //   Humanoid.call(this, characteristic);
+    // }
+    //   Hero.prototype = Object.create(Humanoid.prototype);
+    //   Hero.prototype.slaps = function(character,damage){
+    //     character.healthPoints = character.healthPoints - this.damage;
+    //     return `${this.name} slaps ${character.name} for ${damage} damage!`;
+    //   };
+      
+    // const goblin = new Villain({
+    //   createdAt: new Date(),
+    //   dimensions: {
+    //     length: 1,
+    //     width: .5,
+    //     height: .5,
+    //   },
+    //   healthPoints: 8,
+    //   name: 'Snotnose',
+    //   team: 'Dank Clan',
+    //   weapons: [
+    //     'dagger',
+    //   ],
+    //   language: 'goblin',
+    // });
+  
+  
+     
+    // const warrior = new Hero({
+    //   createdAt: new Date(),
+    //   dimensions: {
+    //     length: 2,
+    //     width: 1.5,
+    //     height: 1,
+    //   },
+    //   healthPoints: 8,
+    //   name: 'John',
+    //   team: 'Mercenary',
+    //   weapons: [
+    //     'pike',
+    //   ],
+    //   language: 'common',
+    // });
+  
+    
+    // console.log(warrior.slaps(goblin, 2));
+    
+    // console.log(goblin.smack(warrior, 2));
+  
